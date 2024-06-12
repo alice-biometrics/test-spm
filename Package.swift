3eb8fc00aadb189dc405add7aa7b2c3a77744152
garciagarciaadrian <agarcia@alicebiometrics.com>
2024-06-12 13:26:42 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "2.3.4"
+let checksum = "8988610609051b2337b569a8cf8d51a62734f8cf69671de48c320eb6d11a47c9"
+
+let package = Package(
+    name: "AliceOnboarding",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "AliceOnboarding",
+            targets: ["AliceOnboardingBundle"]),
+    ],
+    targets: [
+        .target(
+            name: "AliceOnboardingBundle",
+            dependencies: ["AliceOnboarding"]
+        ),
+        .binaryTarget(
+            name: "AliceOnboarding",
+            url: "https://storage.googleapis.com/alicebiometrics.com/releases/ios/AliceOnboarding-\(version).zip",
+            checksum: "\(checksum)"
+        ),
+    ]
+)
